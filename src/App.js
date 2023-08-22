b2d8a1f4e1402f32e0058eed7ee119c70656673e
Dan Rice <dan.rice.92@outlook.com>
2023-08-21 22:27:42 -0600

    Begin The Legend of Zelda!

--- src/App.js
+++ src/App.js
@@ -2,16 +2,19 @@ import 'App.scss';
 import midnasHelmet from 'midnas-helmet.png';
 
 import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
 import Navbar from 'react-bootstrap/Navbar';
+
+import TheLegendOfZelda from 'guides/the_legend_of_zelda';
 
 function App() {
   return (
     <Container fluid>
-      <Navbar className="bg-body-tertiary">
+      <Navbar className="bg-body-tertiary mb-3">
         <Container>
           <Navbar.Brand href="/">
-            <img
-              alt=""
+            <Image
+              alt="Midna's Guides logo"
               src={midnasHelmet}
               width="30"
               height="30"
@@ -21,6 +24,7 @@ function App() {
           </Navbar.Brand>
         </Container>
       </Navbar>
+      <TheLegendOfZelda />
     </Container>
   );
 }
---
+++ src/guides/the_legend_of_zelda.js
@@ -0,0 +1,27 @@
+import Accordion from 'react-bootstrap/Accordion';
+import Container from 'react-bootstrap/Container';
+import Image from 'react-bootstrap/Image';
+
+import theLegendOfZeldaLogo from 'the-legend-of-zelda-logo.png';
+
+export default function TheLegendOfZelda() {
+  return (
+    <Container fluid>
+      <Image
+        fluid
+        alt="The Legend of Zelda logo"
+        src={theLegendOfZeldaLogo}
+        className="logo"
+      />
+      <Accordion defaultActiveKey="0">
+        <Accordion.Item eventKey="0">
+          <Accordion.Header>Introduction</Accordion.Header>
+          <Accordion.Body>
+            Welcome to the Midna's Guide to <i>The Legend of Zelda</i>. Open a
+            section to get started.
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    </Container>
+  );
+}
